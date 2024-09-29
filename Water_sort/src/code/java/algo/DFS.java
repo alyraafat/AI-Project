6480c906db97941d97f9c74a1134ac0bb076663f
basel68 <baselfarouk09@gmail.com>
2024-09-29 22:07:47 +0300

    DFS class addition

---
+++ Water_sort/src/code/java/algo/DFS.java
@@ -0,0 +1,32 @@
+package algo;
+
+import java.util.Stack;
+
+public class DFS extends QingFun {
+    private Stack<Node> stack;
+
+    public DFS() {
+        this.stack = new Stack<>();
+    }
+
+    @Override
+    public void addNode(Node node) {
+        stack.push(node);
+    }
+
+    @Override
+    public Node removeNode() {
+        return stack.pop();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    @Override
+    public int evalFunc(int layersPoured) {
+
+        return layersPoured;
+    }
+}
